cdf8e154202ef3f67a81025e1545de2291b4b9be
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-09-27 17:45:47 +0300

    commit tree 8c75e1c16f633ff0ea6c4ebf675f59229e0138be

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+ 
+    #GET/plants
+    get '/plants', to: 'plants#index'
+
+    #GET/plants
+    get '/plants/:id', to: 'plants#show'
+
+    #POST/plants
+
+    post '/plants', to: 'plants#create'
+
 end
